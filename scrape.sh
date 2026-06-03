f7027dab4c55cb8d276e3c3fae0774e73c5ff127
Phil Mocek <phil@mocek.org>
2026-06-02 23:22:52 -0700

    Configure scraper for communityconnectseattle.org target pages

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-# ./download.sh 'https://www.example.com/'
+./download.sh 'https://communityconnectseattle.org/'
+./download.sh 'https://communityconnectseattle.org/camera-registration/'
+./download.sh 'https://communityconnectseattle.org/camera-integration/'
+./download.sh 'https://communityconnectseattle.org/join/'
+./download.sh 'https://communityconnectseattle.org/privacy-policy/'
