4470c6ee8ffe8ac26d549affef3b3f38c4b01e51
Phil Mocek <phil@mocek.org>
2026-07-01 09:54:46 -0700

    feat: integrate scrape-fusus.py into scrape pipeline

--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Fetch camera stats for all Fusus communities
+./scrape-fusus.py
+
+# Download Seattle specific web pages for page diff tracking
 ./download.sh 'https://communityconnectseattle.org/'
 ./download.sh 'https://communityconnectseattle.org/camera-registration/'
 ./download.sh 'https://communityconnectseattle.org/camera-integration/'
 ./download.sh 'https://communityconnectseattle.org/join/'
 ./download.sh 'https://communityconnectseattle.org/privacy-policy/'
+
